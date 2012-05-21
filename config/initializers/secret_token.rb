45b9198295ee29445aabdfc24f7028a55d55b32e
raveclassic-home <raveclassic@gmail.com>
2012-05-22 00:35:46 +0400

    recreated without active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vibes::Application.config.secret_token = 'bcce1718843241dd6d61e6ee9752308f3430523f07de2f249f9f0afee8a0f3aba236913488cbc41a261429900836d39335196ec1a6c4a5d54930a16b2ef920ab'
