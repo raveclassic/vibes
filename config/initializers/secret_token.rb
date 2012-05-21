355a1613ae29b9660176e0d2c8881ab4e5eea18a
raveclassic-home <raveclassic@gmail.com>
2012-05-22 00:32:00 +0400

    deleted

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Vibes::Application.config.secret_token = '18f00a5e1b482dbf3e399afd90d811ce87cef321af5d2c2e1cbb3621fc972c5a42c9d2df0285ba89c5869fe2fe24c58aa52d0a1add253827981459c15d560d72'
